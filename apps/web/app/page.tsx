82ff0000a02e8eb2dcd4de1ab7a6a25b4b11dabc
colpitts-dev <adam@colpitts.dev>
2023-10-12 09:19:20 -0300

    wip: @evoke-ui/react

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,31 @@
 import { getSession } from '@auth0/nextjs-auth0'
 import { redirect } from 'next/navigation'
+import { Card } from '@evoke-ui/react'
 import styles from './page.module.css'
+
+const LINKS = [
+  {
+    title: 'Docs',
+    href: 'https://turbo.build/repo/docs',
+    description: 'Find in-depth information about Turborepo features and API.',
+  },
+  {
+    title: 'Learn',
+    href: 'https://turbo.build/repo/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.',
+  },
+  {
+    title: 'Templates',
+    href: 'https://turbo.build/repo/docs/getting-started/from-example',
+    description: 'Choose from over 15 examples and deploy with a single click.',
+  },
+  {
+    title: 'Deploy',
+    href: 'https://vercel.com/new',
+    description:
+      ' Instantly deploy your Turborepo to a shareable URL with Vercel.',
+  },
+]
 
 export default async function Index(): Promise<JSX.Element> {
   const session = await getSession()
@@ -14,11 +39,27 @@ export default async function Index(): Promise<JSX.Element> {
         </a>
       </header>
       <main className={styles.main}>
-        <h1>
-          <span className="text-primary">Hyper</span>
-          <span className="text-primary-variant">local</span>
-        </h1>
-        <p>Public Landing Page</p>
+        <section className="bg-yellow-300">
+          <h1>
+            <span className="text-primary">Hyper</span>
+            <span className="text-primary-variant">local</span>
+          </h1>
+          <p>Public Landing Page</p>
+        </section>
+        <section className="bg-yellow-300">
+          <div className={styles.grid}>
+            {LINKS.map(({ title, href, description }) => (
+              <Card
+                className={styles.card}
+                href={href}
+                key={title}
+                title={title}
+              >
+                {description}
+              </Card>
+            ))}
+          </div>
+        </section>
       </main>
     </>
   )
