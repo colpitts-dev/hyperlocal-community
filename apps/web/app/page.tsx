80a5515569919dd98e5c2cbf37cb37ae8ca40c04
acolpitts <adam@colpitts.dev>
2023-10-14 13:17:15 -0300

    feat: add evoke-ui design system and component lib

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,17 @@ import styles from './page.module.css'
 
 const LINKS = [
   {
+    title: 'Communities',
+    href: 'https://vercel.com/new',
+    description:
+      'Manage your communities with ease with our powerful dashboard.',
+  },
+  {
+    title: 'Memberships',
+    href: 'https://turbo.build/repo/docs/getting-started/from-example',
+    description: 'Manage memberships for all your communities in one place.',
+  },
+  {
     title: 'Docs',
     href: 'https://turbo.build/repo/docs',
     description: 'Find in-depth information about Hyperlocal features and API.',
@@ -13,18 +24,6 @@ const LINKS = [
     title: 'Learn',
     href: 'https://turbo.build/repo/docs/handbook',
     description: 'Learn more about community engagement with our handbook.',
-  },
-  {
-    title: 'Memberships',
-    href: 'https://turbo.build/repo/docs/getting-started/from-example',
-    description:
-      'Manage memberships for all your online communities with ease.',
-  },
-  {
-    title: 'Blockchain',
-    href: 'https://vercel.com/new',
-    description:
-      'Leverages Blockchain technology to allow community members to trade products and services',
   },
 ]
 
@@ -42,7 +41,7 @@ export default async function Index(): Promise<JSX.Element> {
       <main className={styles.main}>
         <section className="mb-12 text-center">
           <h1 className="text-primary">Hyperlocal</h1>
-          <p>Public Landing Page</p>
+          <p>Community Engement Platform</p>
         </section>
         <section className={`${styles.grid} space-x-4`}>
           {LINKS.map(({ title, href, description }) => (
