5e79281b744029705fb13e1b4b21dff4eb60adc0
colpitts-dev <adam@colpitts.dev>
2023-10-14 12:43:52 -0300

    fix: disable eslint on auth0 dynamic routes

--- apps/auth0-react/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders a login button', () => {
-  render(<App />)
-  const loginButton = screen.queryByTestId('login-button')
-  expect(loginButton).toBeInTheDocument()
-})
