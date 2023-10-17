a65621762377f0d67eb17898d078f4cb4d4fff6a
colpitts-dev <adam@colpitts.dev>
2023-10-17 00:37:11 -0300

    feat: add GET memberships routes

---
+++ services/membership-service/src/handlers/getMemberships.ts
@@ -0,0 +1,34 @@
+import createError from 'http-errors'
+import { Membership } from '@hyperlocal/models'
+import { connectMongoDB } from '../lib/mongoDB'
+import { commonMiddleware } from '../lib/commonMiddleware'
+import { getErrorMessage } from '../lib/errors'
+
+async function getMemberships(event: any, context: any) {
+  // Make sure to add this so you can re-use `conn` between function calls.
+  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+  context.callbackWaitsForEmptyEventLoop = false
+  try {
+    // Connect to mongodb
+    await connectMongoDB()
+
+    // Get all memberships
+    const memberships = await Membership.find()
+    if (!memberships) {
+      throw new createError.NotFound(`No memberships found!`)
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(memberships),
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify(getErrorMessage(error)),
+    }
+  }
+}
+
+export const handler = commonMiddleware(getMemberships)
